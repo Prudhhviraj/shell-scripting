2fb2bb6d3574de6afdae51cb841bf0b140ee7b36
prudh <prudhhviraj2@gmail.com>
2025-03-16 19:36:25 +0530

    Arrays and how it works

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+MOVIES=("PUSHPA" "RRR" "DEVARA")
+# Index start from 0 and size is 3
+
+echo "first movie is ${MOVIES[0]}"
+echo "second movie is ${MOVIES[1]}"
+echo "Third Movie is ${MOVIES[2]}"
+
+echo "All Movies are ${MOVIES[@]}"
