6d421cb496233347fbdbc24503d2cee9e57ecc70
prudh <prudhhviraj2@gmail.com>
2025-03-16 19:51:23 +0530

    Special Variables in shell scripting

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+$A=$HOME
 
 echo "All variables passed: $@"
 echo "Number of variables:$#"
@@ -7,5 +8,6 @@ echo "Script Name: $0"
 echo "Present Working Directory: $PWD"
 echo "Which user is running this script: $USER"
 echo "Process Id of Current Script: $$"
+echo "Home Directory of user: $A"
 sleep 60 &
 echo "Process Id of last command in Background: $!"
