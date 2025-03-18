05856b887095ebf3108cb31082c232c785277514
prudh <prudhhviraj2@gmail.com>
2025-03-18 18:59:53 +0530

    Condition if else

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -3,8 +3,8 @@
 NUMBER1=$1
 NUMBER2=$2
 
-TIMESTAMP=$(date)
-echo "your script has been executed at: $TIMESTAMP"
+PRUDHVI=$(date)
+echo "your script has been executed at: $PRUDHVI"
 SUM=$((NUMBER1+NUMBER2))
 
 echo "sum of $NUMBER1 and $NUMBER2 is $SUM"
