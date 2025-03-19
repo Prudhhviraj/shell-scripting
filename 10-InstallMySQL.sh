e83d800411586130af7754116b36107cd14f79e0
prudh <prudhhviraj2@gmail.com>
2025-03-19 16:47:31 +0530

    added another name

--- 10-InstallMySQL.sh
+++ 10-InstallMySQL.sh
@@ -1,31 +1,34 @@
 #!bin/bash
 
+APP1=$(mysql)
+APP2=$(git)
+STATUS=$(dnf list installed $APP1 and $APP2)
 
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Need Root access to install mysql"
+    echo "Need Root/admin access for installations"
     exit 1
 fi
 
-dnf list installed mysql &
+$STATUS
 
 if [ $? -ne 0 ]
 then 
-    dnf install mysql -y
+    dnf install $APP1 -y
     if [ $? -ne 0 ] 
     then 
     echo "Error:: Failed to Install My SQL"
     exit 1
     else 
-    echo "Success:: MysQL has been installed Successfully"
+    echo "Success:: $APP1 has been installed Successfully"
     fi
 else    
-    echo "MySQL Already Installed"
+    echo "$APP1 Already Installed"
 fi
 
-dnf list installed git &
+$STATUS
 
 if [ $? -ne 0 ]
 then
