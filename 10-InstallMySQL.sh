33093c963030c754f3e464a5af44e5209b714810
prudh <prudhhviraj2@gmail.com>
2025-03-19 16:25:42 +0530

    added another name

--- 10-InstallMySQL.sh
+++ 10-InstallMySQL.sh
@@ -9,7 +9,7 @@ then
     exit 1
 fi
 
-dnf list installed mysql
+dnf list installed mysql &
 
 if [ $? -ne 0 ]
 then 
@@ -25,7 +25,7 @@ else
     echo "MySQL Already Installed"
 fi
 
-dnf list installed git
+dnf list installed git &
 
 if [ $? -ne 0 ]
 then
