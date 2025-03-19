7c2c99b29283e595bb9f42ea2b1dacb548efc3d5
prudh <prudhhviraj2@gmail.com>
2025-03-19 12:56:17 +0530

    mysql installation

--- 10-InstallMySQL.sh
+++ 10-InstallMySQL.sh
@@ -6,6 +6,24 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Need Root access to install mysql"
-else
-    dnf install mysql
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0]
+then 
+    echo "Error:: Failed to Install My SQL"
+    exit 1
+else 
+    echo "Success:: MysQL has been installed Successfully"
+fi
+
+dnf install git -y
+
+if [$? -ne 0]
+then 
+    echo "Error:: Failed to install GIT"
+else 
+    echo "Success:: GIT has been installed Successfully"
 fi
